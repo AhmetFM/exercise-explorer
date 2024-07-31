ed2f3759b90082a2d90bfc6779220b272f7bf6c3
Ahmet Furkan Meric <afm634@gmail.com>
2024-08-01 01:43:29 +0300

    Updated ai api key

--- src/app/(routes)/(steps)/step-three/actions.ts
+++ src/app/(routes)/(steps)/step-three/actions.ts
@@ -4,7 +4,7 @@ import {
 } from "@google/generative-ai";
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyDJXog-0Eb_VyuANdwkgEaaOtCihQHhkLQ");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-pro",
